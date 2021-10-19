74ec3667a7b9fa2d787a3295b9d339f664a19168
Sergey Ivanov <ivanovs-4@yandex.ru>
2021-10-19 09:42:01 +0400

    Update flake to use hakell-flake-utils

--- flake.nix
+++ flake.nix
@@ -4,51 +4,12 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
+    haskell-flake-utils.url = "github:ivanovs-4/haskell-flake-utils";
   };
 
-  outputs = { self, ... }@inputs:
-    inputs.flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = inputs.nixpkgs.legacyPackages.${system};
-
-        packageName = "{appname}";
-
-        haskellPackages = pkgs.haskellPackages.override {
-          inherit ((self.overlay.${system} {} pkgs).${packageName}) overrides;
-        };
-
-        chain-overrides = new: old:
-          builtins.foldl' (upd: overrides: upd // overrides new (old // upd)) old;
-
-        overrides = new: old: {
-          ${packageName} = old.callCabal2nix packageName self {
-          };
-        };
-
-      in {
-
-        overlay = final: prev: {
-          ${packageName}.overrides = new: old: chain-overrides new old [
-            overrides
-          ];
-        };
-
-        packages.${packageName} = haskellPackages.${packageName};
-
-        defaultPackage = self.packages.${system}.${packageName};
-
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs.haskellPackages; [
-            ghcid
-            cabal-install
-
-            (pkgs.haskellPackages.ghcWithPackages (h: with h; [
-            ]))
-
-          ];
-          inputsFrom = [ self.defaultPackage ];
-          # inputsFrom = builtins.attrValues self.packages.${system};
-        };
-      });
+  outputs = { self, nixpkgs, ... }@inputs:
+    inputs.haskell-flake-utils.lib.simpleCabal2flake {
+      inherit self nixpkgs;
+      name = "{appname}";
+    };
 }
