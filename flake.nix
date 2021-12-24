718086d6e8a9905c9e4e021aee8e5d4daf80c44c
Sergey Ivanov <ivanovs-4@yandex.ru>
2021-12-24 19:22:28 +0400

    Tune defaults, use haskell-flake-utils

--- flake.nix
+++ flake.nix
@@ -5,11 +5,15 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
     haskell-flake-utils.url = "github:ivanovs-4/haskell-flake-utils";
+
   };
 
   outputs = { self, nixpkgs, ... }@inputs:
-    inputs.haskell-flake-utils.lib.simpleCabal2flake {
+    with inputs.haskell-flake-utils.lib;
+    simpleCabal2flake {
       inherit self nixpkgs;
+
       name = "{appname}";
+
     };
 }
--- test/AppnameTest.hs
+++ test/AppnameTest.hs
@@ -1,6 +1,6 @@
 module Main (main) where
 
-import {Appname}
+-- import {Appname}
 
 main :: IO ()
 main = putStrLn "Test suite not yet implemented."
