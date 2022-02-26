9233fbadf9c6f83bad0cb28040a78fc064b1a5ec
Sergey Ivanov <ivanovs-4@yandex.ru>
2022-02-27 01:09:59 +0400

    Use updated haskell-flake-utils

--- flake.nix
+++ flake.nix
@@ -1,19 +1,22 @@
 {
   description = "{appname}";
-  nixConfig.bash-prompt = ''[nix-develop]$ '';
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
     haskell-flake-utils.url = "github:ivanovs-4/haskell-flake-utils";
+    haskell-flake-utils.inputs.nixpkgs.follows = "nixpkgs";
+    haskell-flake-utils.inputs.flake-utils.follows = "flake-utils";
 
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
-    with inputs.haskell-flake-utils.lib;
-    simpleCabal2flake {
-      inherit self nixpkgs;
+outputs = { self, nixpkgs, flake-utils, haskell-flake-utils, ... }@inputs:
+  flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+    inputs.haskell-flake-utils.lib.simpleCabal2flake {
+      inherit self nixpkgs system;
 
       name = "{appname}";
 
-    };
+    }
+  );
 }
